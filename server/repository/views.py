fc0ffddf569602abb109496f61d697eb43acdb4e
Eduardo Gamboa <gamboaeduardo00@gmail.com>
2024-11-01 02:25:55 -0600

    fix: se arregló un pequeño error en la vista del formulario

--- server/repository/views.py
+++ server/repository/views.py
@@ -1,12 +1,12 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CreateRepo
+from .forms import RepoForm
 
 
 # Create your views here.
 class CreateRepoView(generic.FormView):
     template_name = 'repository/create.html'
-    form_class = CreateRepo
+    form_class = RepoForm
     success_url = reverse_lazy('repo_create')
 
     def form_valid(self, form):

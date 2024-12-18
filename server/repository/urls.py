407a0831dc8694f4025472f421f9caa1ba657107
EduardoGamboaHernandez <gamboaeduardo00@gmail.com>
2024-12-17 23:51:57 -0600

    fix: arreglo de un error en la url para la vista de los repositorios

--- server/repository/urls.py
+++ server/repository/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', home, name="repo_to_home"),
     path('crear/', CreateRepoView.as_view(), name="repo_create"),
     path('<str:username>/<str:repo>', ShowRepoView.as_view(), name="repo_show"),
-    path(r'<username>/$', ShowListRepoView.as_view(), name="repo_list"),
+    path(r'<username>/', ShowListRepoView.as_view(), name="repo_list"),
+    path('<str:username>/', ShowListRepoView.as_view(), name="repo_lis"),
     path('commits/<str:username>/<str:repo>/', ListCommitsView.as_view(), name="repo_list_commits"),
 ]

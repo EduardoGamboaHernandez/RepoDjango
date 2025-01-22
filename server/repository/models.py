0b126a7852e3b865bd5ebdd7676f1737a957c894
EduardoGamboaHernandez <gamboaeduardo00@gmail.com>
2025-01-22 13:27:44 -0600

    docs: agregar comentarios para la clase de models.py

--- server/repository/models.py
+++ server/repository/models.py
@@ -12,6 +12,9 @@ REPOS_DIR = getattr(settings, "REPOS_DIR", None)
 
 
 class RepoModel(models.Model):
+    """
+    Modelo de la tabla que almacena información de los repositorios
+    """
     hashid = HashidsField(real_field_name="id", min_length=6)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
@@ -24,8 +27,8 @@ class RepoModel(models.Model):
 
     def save(self, *args, **kwargs):
         path = os.path.join(REPOS_DIR, self.user.username)
+        # Crear el repositorio
         repo = Bare(self.name, path)
-
         repo.create(self.description, self.remote)
 
         return super().save(*args, **kwargs)
@@ -33,6 +36,10 @@ class RepoModel(models.Model):
 
 @receiver(models.signals.post_delete, sender=RepoModel)
 def delete_repo_hook(sender, instance, using, **kwargs):
+    """
+    función que funciona como gancho al borrar un
+    elemento de la base de datos también borrar el archivo .git
+    """
     path = os.path.join(REPOS_DIR, instance.user.username, f"{instance.name}.git")
     try:
         shutil.rmtree(path)
